b6fc81170fc31ca3fe36a297dc5fad92fcb0a57f
Taxil1810 <taxilsakariya@gmail.com>
2025-06-12 14:16:57 +0530

    project.py

---
+++ diamond.py
@@ -0,0 +1,8 @@
+for i in range(5):
+    print(' ' * (5 - i - 1) + '*' *(i+1))
+
+for i in range(5 - 2 , -1 , -1):                 
+    print(' ' * (5 - i - 1) + '*' *(i+1))
+
+for i in range(5 - 2 , -1 , -1):
+    print(' ' * (5 + i + 1) + '*' *(i+1))
